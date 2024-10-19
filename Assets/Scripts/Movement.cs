15319e8be7ea4ad89153abb0883d31cf1bcdd281
davidmooers <davidmooers@uvic.ca>
2024-10-19 10:32:09 -0700

    level bounds

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,15 +5,18 @@ public class Movement : MonoBehaviour
 
     [SerializeField] public float moveSpeed = 1.0f;
     [SerializeField] public float rotationSpeed = 1.0f;
+    [SerializeField] public GameObject levelBackground;
 
     public Vector2 movementDirection = new Vector2(0, 0);
     public float targetRotationAngle = 0.0f;
 
     private Rigidbody2D rb;
+    private SpriteRenderer levelBackgroundRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        levelBackgroundRenderer = levelBackground.GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
@@ -35,6 +38,15 @@ public class Movement : MonoBehaviour
         // );
     }
 
+    void LateUpdate()
+    {
+        float distPastBounds = GetDistancePastLevelBounds();
+        if (distPastBounds > 0) {
+            Vector2 directionToCenter = Vector2.zero - rb.position;
+            rb.AddForce(moveSpeed * distPastBounds/100 * directionToCenter);
+        }
+    }
+
     public void SetMoveDirection(Vector2 newDirection)
     {
         this.movementDirection = newDirection;
@@ -43,5 +55,14 @@ public class Movement : MonoBehaviour
     public void SetTargetRotation(float newTargetRotationAngle)
     {
         this.targetRotationAngle = newTargetRotationAngle;
+    }
+
+    public float GetDistancePastLevelBounds()
+    {
+        Bounds bounds = levelBackgroundRenderer.bounds;
+        float radius = bounds.extents.x;
+        float distanceFromCenter = rb.position.magnitude;
+        return Mathf.Max(distanceFromCenter - radius, 0);
+
     }
 }
